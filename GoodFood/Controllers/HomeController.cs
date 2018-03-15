339174a139d29dff874bee9e7f4ee6cf76850cba
Calo10 <carlos_mendez_10@live.com>
2018-03-15 06:10:32 -0600

    Switching comouter

--- GoodFood/Controllers/HomeController.cs
+++ GoodFood/Controllers/HomeController.cs
@@ -29,6 +29,13 @@ namespace GoodFood.Controllers
             return View();
         }
 
+        public IActionResult RegisterUser()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
